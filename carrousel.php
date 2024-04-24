e4b8cbe11a061aef436836cd17113a103e07f591
Rousseau <e2173165@cmaisonneuve.qc.ca>
2024-04-24 10:38:22 -0400

    le clique radio fonctionne pour le changement d'image

--- carrousel.php
+++ carrousel.php
@@ -8,37 +8,39 @@
      */
 
     // Ajout d'un shortcode
-    add_shortcode('carrousel', 'genere_html');
-
-        function enqueue_style_script() {
-            $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
-            $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
-
-            wp_enqueue_style(   'em_plugin_carrousel_css',
-                plugin_dir_url(__FILE__) . "style.css",
-                array(),
-                $version_css);
-
-            wp_enqueue_script(  'em_plugin_carrousel_js',
-                plugin_dir_url(__FILE__) ."js/carrousel.js",
+    
+    function enqueue_style_script() {
+        $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
+        $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+        
+        wp_enqueue_style(   'em_plugin_carrousel_css',
+        plugin_dir_url(__FILE__) . "style.css",
+        array(),
+        $version_css);
+        
+        wp_enqueue_script(  'em_plugin_carrousel_js',
+        plugin_dir_url(__FILE__) ."js/carrousel.js",
                 array(),
                 $version_js,
                 true);
                 // true pour mettre le script en bas de page
-        }
-
-        add_action('wp_enqueue_scripts', 'enqueue_style_script');
-
-        function genere_html() {
-            $html = '
-            <button class="bouton__ouvrir">Ouvrir Carrousel</button>
-            <div class="carrousel">
-                <a href="" class="carrousel__x">X</a>
+            }
+            
+            add_action('wp_enqueue_scripts', 'enqueue_style_script');//wp_head doit être appelé juste avant la balise fermante </head> de votre thème et wp_footer doit être appelé juste avant la balise fermante </body> de votre thème.
+            
+            function genere_html() {
+                $html = '
+                <button class="bouton__ouvrir">Ouvrir Carrousel</button>
+                <div class="carrousel">
+                <button class="carrousel__x">X</button>
                 <figure class="carrousel__figure"></figure>
                 <form action="" class="carrousel__form"></form>
-            </div>
-            ';
-            return $html;
-        }
+                </div>
+                ';
+                return $html;
+            }
+            //[carrousel] juste après le contenu de l'article
+            //Quand la fonction the content est appelée, elle affichera le shortcode.
+            add_shortcode('carrousel', 'genere_html');
             ?>
 
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,70 @@
+
+
+
+
 (function(){
     console.log("vive JS");
+    let carrousel = document.querySelector('.carrousel');
+    let bouton = document.querySelector('.bouton__ouvrir');
+    let boutonFermer = document.querySelector('.carrousel__x');
+    let galerie = document.querySelector('.galerie');
+    console.log(galerie.tagName);
+    let carrousel__figure = document.querySelector('.carrousel__figure');
+    /*création dynamique d'img dans le carrousel*/ 
+    let form = document.querySelector('.carrousel__form');
+
+
+    //let galerie__img = galerie.querySelector('img');
+    //console.log(galerie__img.src);
+    //carrousel__img.src = galerie__img.src;
+    //console.log(carrousel__img.src);
+    
+    
+    let galerie__imgs = galerie.querySelectorAll('img');
+    console.log(galerie__imgs);
+
+    let index = 0;
+
+    for (const elm of galerie__imgs) {
+        console.log(elm.src);
+        creerImageCarrousel(elm,index)
+        creerRadioCarrousel(index);
+        index++;
+    }
+
+    function creerRadioCarrousel(index){
+        let carrousel__radio = document.createElement('input');
+        carrousel__radio.classList.add("carrousel__radio");
+        carrousel__radio.type = 'radio';
+        carrousel__radio.name = 'radioCarrousel';
+        carrousel__radio.dataset.index = index;
+        form.appendChild(carrousel__radio);
+        //ajouter un écouteur permettant de changer l'image du carrousel selon l'index du radio
+        //carrousel__img.children[index].sttyle.opacity = 1; pour changer l'opacité de l'image
+        carrousel__radio.addEventListener('click', function(){
+            let index = this.dataset.index;
+            let carrousel__imgs = document.querySelectorAll('.carrousel__img');
+            for (const img of carrousel__imgs) {
+                img.style.opacity = 0;
+            }
+            carrousel__imgs[index].style.opacity = 1;
+        });
+    }
+
+    function creerImageCarrousel(elm,index){
+        let carrousel__img = document.createElement('img');
+        carrousel__img.classList.add("carrousel__img");
+        carrousel__img.src = elm.src;
+        carrousel__img.dataset.index = index;
+        carrousel__figure.appendChild(carrousel__img);
+    }
+
+
+    bouton.addEventListener('click', function(){
+        carrousel.classList.add('carrousel--ouvrir');
+    });
+
+    boutonFermer.addEventListener('click', function(){
+        carrousel.classList.remove('carrousel--ouvrir');
+    });
 })();
